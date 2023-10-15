fe13f1288d984765d9836f5629c563676eaab2b7
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-10-15 19:21:21 +0530

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -1,15 +1,31 @@
-const mongoose = require('mongoose');
+ const mongoose = require('mongoose');
 
 // Define the schema for the 'Product' model
 const productSchema = new mongoose.Schema({
   // 'name' field is of type String
   // It is required (cannot be empty)
+  name: {
+    type: String,
+    required: true
+  },
   // 'price' field is of type Number
   // It is required (cannot be empty)
+   price: {
+    type: Number,
+    required: true
+  },
   // 'category' field is of type String
   // It is required (cannot be empty)
+  category: {
+    type: String,
+    required: true
+  },
   // 'availability' field is of type Boolean
   // It has a default value of 'true'
+  availability: {
+    type: Boolean,
+    default: true
+  }
 });
 
 // Create a 'Product' model using the defined schema
